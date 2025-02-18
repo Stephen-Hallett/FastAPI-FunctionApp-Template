32d473f5fce58804c3d941cf0da85e390f82174c
Stephen-Hallett <stevohallett@gmail.com>
2025-02-18 22:33:36 +1300

    feat: Initial commit

---
+++ infra/deploy/dev.tfvars
@@ -0,0 +1,10 @@
+env             = "dev"
+loc             = "eau"
+subscription_id = "xxxxxxxxxxxxxxxxx"
+
+# App service plan
+tier = "Dynamic"
+sku = "Y1"
+
+# Function app
+always_on = false
---
+++ infra/deploy/variables.tf
@@ -0,0 +1,44 @@
+# App service plan
+variable "tier" {
+  type = string
+}
+
+variable "sku" {
+  type = string
+}
+
+# Function app
+variable always_on {
+  type = bool
+}
+
+#AZURE VARIABLES
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+
+#COMMON VARIABLES
+variable "project_id" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "loc" {
+  type = string
+}
